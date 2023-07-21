62513fa1e331507690859431ea3bf1e4369b9bbc
Antonin <flowythe@gmail.com>
2023-07-22 01:33:22 +0200

    updated music box

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,5 +1,3 @@
-var api = "https://api.gangnamstyle.dev/lastfm";
-
 function isElementOverflowing(element) {
     var overflowX = element.offsetWidth < element.scrollWidth,
         overflowY = element.offsetHeight < element.scrollHeight;
@@ -8,24 +6,48 @@ function isElementOverflowing(element) {
 }
 
 // get elements by id
-const title = document.getElementById("musicbox_title");
-const cover = document.getElementById("musicbox_cover");
+var title = document.getElementById("musicbox_title");
+var songlink = document.getElementById("musicbox_songlink");
+var cover = document.getElementById("musicbox_cover");
 
-const song = document.getElementById("musicbox_song");
-const artist = document.getElementById("musicbox_artist");
-const album = document.getElementById("musicbox_album");
+var song = document.getElementById("musicbox_song");
+var artist = document.getElementById("musicbox_artist");
+var album = document.getElementById("musicbox_album");
 
-const listening = document.getElementById("musicbox_listening");
-const listens = document.getElementById("musicbox_listens");
+var listening = document.getElementById("musicbox_listening");
+var listens = document.getElementById("musicbox_listens");
 
 function main() {
     // get lastfm data from api
-    fetch(api)
+    fetch("https://api.gangnamstyle.dev/lastfm")
         .then(response => response.json())
         .then(data => {
             var e = data.recenttracks.track[0];
 
-            title.innerHTML = "ye";
+            if (e["@attr"] && e["@attr"].nowplaying == "true") {
+                title.innerHTML = "Now Playing:";
+                listening.innerHTML = "Listening right now!";
+            } else {
+                title.innerHTML = "Last Played:";
+
+                // get the last played time
+                var lastPlayed = new Date(e.date.uts * 1000);
+                var lastPlayedString = lastPlayed.toLocaleString("en-US", {
+                    // get the time in the user's
+                    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                    second: "numeric"
+                });
+                listening.innerHTML = `Last played ${lastPlayedString}`;
+            
+
+            }
+
             cover.src = e.image[3]["#text"];
 
             song.innerHTML = e.name;
@@ -33,7 +55,7 @@ function main() {
             album.innerHTML = e.album["#text"];
 
             listening.innerHTML = data.status;
-            listens.innerHTML = data.recenttracks["@attr"].total;
+            listens.innerHTML = `${data.recenttracks["@attr"].total} listens`;
 
             // check if text is overflowing
             if (isElementOverflowing(song)) {
