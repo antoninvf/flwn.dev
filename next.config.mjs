0642639649c986f16cb9fe3420c3d30d6925fe24
Antonin <flowythe@gmail.com>
2025-02-28 16:32:52 +0100

    Surely this will fix it

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
-  },
+	reactStrictMode: true,
+	output: 'standalone',
+	experimental: {
+		optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+	},
 };
 
 export default nextConfig;
