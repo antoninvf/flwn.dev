e492d663178ddbce2ac51c7afb792cf791556e0a
Antonin <av@flwn.dev>
2026-07-03 00:33:33 +0200

    temporary work in progress page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,44 +14,46 @@
 		{@render children()}
 	</main>
 
-	<footer>
-		<p>
-			breaker by <a href="https://github.com/flowyan">flowyan</a>
-		</p>
-	</footer>
+<!--	<footer>-->
+<!--		<p>-->
+<!--			<a href="https://github.com/flowyan">github</a>-->
+<!--			<a href="https://bsky.app/profile/flwn.dev">bluesky</a>-->
+<!--		</p>-->
+<!--	</footer>-->
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		box-sizing: border-box;
-	}
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+				justify-content: center;
+        align-items: center;
+        padding: 1rem;
+        width: 100%;
+        box-sizing: border-box;
+    }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
-	footer a {
-		font-weight: bold;
-	}
+    footer a {
+        font-weight: bold;
+    }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+    @media (min-width: 480px) {
+        footer {
+            padding: 12px 0;
+        }
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,53 @@
 <script>
-	import { Button } from '$lib';
 	import Logo from '$lib/components/Logo.svelte';
 </script>
 
 <Logo />
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button>click me!</Button>
+
+<div class="module">
+	<h1>work in progress</h1>
+	<p>i am currently remaking my website<br>using svelte this time:)</p>
+	<img src="img/construction.gif" alt="a bunch of different construction gifs" height="300" />
+	<p>check me out on these in the meantime</p>
+	<div class="socials">
+		<a href="https://bsky.app/profile/flwn.dev">bluesky</a>
+		<a href="https://github.com/flowyan">github</a>
+		<a href="https://modrinth.com/user/Flowyan">modrinth</a>
+	</div>
+</div>
+
+<style>
+    .module {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--color-bg);
+        padding: 15px;
+        border: 1px solid white;
+    }
+
+    p {
+        text-align: center;
+    }
+
+    img {
+        margin: 15px;
+    }
+
+    .socials {
+        display: flex;
+        margin-top: 15px;
+        gap: 15px;
+
+        a {
+            padding: 5px 15px;
+            background-color: white;
+
+            &:hover {
+                color: white;
+                background-color: var(--color-text);
+            }
+        }
+    }
+</style>
